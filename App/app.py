f09fb3da33d7cfa76acbb081bf05fe4408f6d9c5
Sumeet Patil <spatil38@binghamton.edu>
2023-05-02 01:26:04 -0400

    alignement and minor code

--- App/app.py
+++ App/app.py
@@ -8,6 +8,7 @@ from pymongo import MongoClient
 import json
 import plotly
 import plotly.express as px
+import plotly.graph_objs as go
 import os
 import datetime
 from collections import Counter
@@ -20,27 +21,60 @@ app = Flask(__name__, template_folder=template_path)
 @app.route('/')
 def transactions():
     url = "http://192.168.1.183:5000/transactions"
+    urlCount = "http://192.168.1.183:5000/transactions/metrics"
+    urlCategory = "http://192.168.1.183:5000/transactions/category/metrics"
     response = requests.get(url)
+    responseCount = requests.get(urlCount)
+    responseCategory = requests.get(urlCategory)
     if response.status_code == 200:
         data = json.loads(response.content)
+        
         transactions = []
         for transaction in data['transactions']:
             if '' in transaction:
                 del transaction['']
             # print(transaction)
-            transactions.append((transaction["trans_date_trans_time"], transaction["amt"]))
-        df = pd.DataFrame(transactions, columns=["trans_date_trans_time", "amt"])
-        #fig = px.line(df, x="timestamp", y="amount")
-        fig = px.line(df, x="trans_date_trans_time", y="amt")
+            transactions.append((transaction["category"], transaction["zip"]))
+        df = pd.DataFrame(transactions, columns=["category", "zip"])
+        fig = px.line(df, x="category", y="zip")
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        return render_template('dashboard.html', transactionsJSON=graphJSON)
+
+        #Graph2: Fraud and non Fraud records
+    fraud_count = 0
+    not_fraud_count = 0
+    total_transactions = 0
+    if response.status_code == 200:
+        dataCount = json.loads(responseCount.content)
+        print(dataCount)
+        fraud_count = dataCount['fraud']
+        not_fraud_count = dataCount['notFraud']
+        total_transactions = dataCount['totalTransactions']
+
+        # Create the donut chart
+        labels = ['Fraud', 'Not Fraud']
+        values = [fraud_count, not_fraud_count]
+
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5)])
+        fig.update_layout(
+        title_text=f"Fraud Transactions: {fraud_count}/{total_transactions} ({fraud_count/total_transactions*100:.2f}%)",
+        annotations=[dict(text='Fraud', x=0.5, y=0.5, font_size=20, showarrow=False)]
+)
+        #Graph3:Histogram of top categories and fraud count
+        categories = []
+        totals = []
+        for result in responseCategory["results"]:
+            categories.append(result["_id"])
+            totals.append(result["total"])
+
+        graphCount = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        return render_template('dashboard.html', transactionsJSON=graphJSON, count=graphCount)
     else:
         print('Error in getting response from the url')
-        return render_template('dashboard.html', transactionsJSON=graphJSON)
+        return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
     #  app.run(debug=True, port=5000, host='0.0.0.0')
-    app.run(host='192.168.1.183', port=8000)
+    app.run(host='192.168.1.183', port=8000, debug=True)
 
 # kill $(lsof -t -i:8080) ---------to kill process at 5000
