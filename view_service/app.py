0e9003d5d9d8fa66671cbc7d1b1aea49a4623c55
Raghuvar arora <raghuvararora@gmail.com>
2023-05-03 15:05:55 -0400

    added route for gender metrics

--- view_service/app.py
+++ view_service/app.py
@@ -224,6 +224,62 @@ def get_transaction_time_metrics():
 
 
 
+@app.route("/transactions/gender/metrics", methods=["GET"])
+def get_transaction_gender_metrics():
+    pipeline = [
+  {
+    "$group": {
+      "_id": "$gender",
+      "count": {
+        "$sum": 1
+      },
+      "total": {"$sum": 1},
+      "is_fraud_count": {
+        "$sum": {
+          "$cond": [
+            {
+              "$eq": [
+                "$is_fraud",
+                "1"
+              ]
+            },
+            1,
+            0
+          ]
+        }
+      }
+    }
+  },
+  {
+    "$project": {
+      "_id": 0,
+      "gender": "$_id",
+    #   "percentage": {
+    #     "$divide": [
+    #       "$is_fraud_count",
+    #       "$count"
+    #     ]
+    #   },
+      "fraud_percentage": {"$trunc": [{"$multiply": [{"$divide": ["$is_fraud_count", "$total"]}, 100]}, 2]}
+    }
+  }
+    ]
+
+    reqstring="/transactions/gender/metrics"
+    try:
+        if not redis_client.exists(reqstring):
+            collection_transactions=db["transactions"]
+            agg_result=collection_transactions.aggregate(pipeline)
+            res=jsonify(results=[result for result in agg_result])
+            redis_client.set(reqstring, res.get_data() )
+            redis_client.expire(reqstring, 10)
+            return res.get_data()
+        else:
+            return redis_client.get(reqstring)
+    except Exception as e:
+        return "server error "+str(e), 500
+
+
 
 
 if __name__ == "__main__":
